1b983fa649dc25c9fab3d013dca32ea057446273
baris <bariserayn@icloud.com>
2022-03-07 16:57:48 +0300

    Initial Commit

---
+++ RaiderIO/RaiderIOApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaiderIOApp.swift
+//  RaiderIO
+//
+//  Created by baris on 7.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct RaiderIOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
